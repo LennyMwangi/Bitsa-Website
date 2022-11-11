ff11d3d54f4e21ce390e9a97dd4be9690ca78148
EvansCoder <wangaevans84@gmail.com>
2022-11-11 09:20:49 +0300

    Removed unused code for dark theme (wrapper.active) classes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,15 @@
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenuBtn = document.querySelector('.mobile-menu')
 const menu= document.querySelector('.navlinks')
 
 
-mobileMenu.addEventListener('click', () => {
+mobileMenuBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+
+// typing effect
+      var typed = new Typed("#typed", {
+        stringsElement: "#typed-strings",
+        typeSpeed: 80,
+        loop: true,
+      });
+    
