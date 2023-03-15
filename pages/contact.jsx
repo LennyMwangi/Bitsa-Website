cf75ea78005adaf11311e159444c49dd962fb246
wangaevans <wangaevans84@gmail.com>
2023-03-15 14:02:48 +0300

    Added contact form functionality

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,8 +1,42 @@
+import { useFormik } from 'formik';
+import { useRouter } from 'next/router';
 import React from 'react'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios'
+import login_validate from '../lib/validate';
 
 const Contact = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      subject: '',
+      email: '',
+      message: ''
+    },
+    // validate: login_validate,
+    onSubmit
+  })
+  const router = useRouter();
+  async function onSubmit(values, { resetForm }) {
+    try {
+      const response = await axios.post("/api/contact", values);
+      console.log(response);
+      if (response.status === 200) {
+        resetForm();
+        toast.success("message sent successfully", { autoClose: 2000 });
+        setTimeout(() => {
+          router.push("/")
+          
+        },2000)
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  };
   return (
     <section className="container mx-auto bg-white ">
+      <ToastContainer />
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-[568px]">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 d">
           Contact Us
@@ -11,10 +45,9 @@ const Contact = () => {
           Got a technical issue? Want to send feedback about a beta feature? Need
           details about how we operate? Let us know.
         </p>
-        <form action="#" className="space-y-8">
+        <form className="space-y-8" onSubmit={formik.handleSubmit}>
           <div>
             <label
-              htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               Your email
@@ -22,13 +55,13 @@ const Contact = () => {
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+              {...formik.getFieldProps('email')}
+              className={`${formik.errors.email && formik.touched.email ? 'border-rose-600' : ''} shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5`}
               placeholder="name@ueab.ac.ke"
             />
           </div>
           <div>
             <label
-              htmlFor="subject"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               Subject
@@ -36,7 +69,9 @@ const Contact = () => {
             <input
               type="text"
               id="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
+              {...formik.getFieldProps('subject')}
+              className={`${formik.errors.subject&& formik.touched.subject? 'border-rose-600' : ''} shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5`}
+              // className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
               placeholder="Let us know how we can help you"
             />
           </div>
@@ -50,9 +85,11 @@ const Contact = () => {
             <textarea
               id="message"
               rows={6}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+              {...formik.getFieldProps('message')}
+              className={`${formik.errors.message && formik.touched.message ? 'border-rose-600' : ''} shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5`}
+
+              // className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
               placeholder="Leave a comment..."
-              defaultValue={""}
             />
           </div>
           <button
