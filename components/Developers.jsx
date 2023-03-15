d19346e99f788458cf408251cfc7666c5ca958d4
Ernest <musaubrian15@gmail.com>
2023-03-15 12:43:42 +0300

    chore: update developers component

--- components/Developers.jsx
+++ components/Developers.jsx
@@ -23,13 +23,13 @@ const Developers = () => {
                         <div
                             role="list"
                             aria-label="Behind the scenes People "
-                            className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around gap-3"
+                            className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around justify-evenly"
                         >
                             {data.developers.map((developer, index) => (
                                 <div
                                     key={index}
                                     role="listitem"
-                                    className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5"
+                                    className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5 md:px-4"
                                 >
                                     <div className="rounded overflow-hidden shadow-md bg-white">
                                         <div className="absolute -mt-20 w-full flex justify-center">
